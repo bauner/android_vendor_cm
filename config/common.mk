7ba18fd5fd48b27b080ac5c986b48cdb02231013
bauner <bauner1981@gmail.com>
2014-09-14 11:27:12 +0200

    disable multithreaded dexopt by default

--- config/common.mk
+++ config/common.mk
@@ -66,9 +66,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
-# enable multithreaded dexopt by default
+# disable multithreaded dexopt by default
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.dalvik.multithread=true
+    persist.sys.dalvik.multithread=false
 
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
