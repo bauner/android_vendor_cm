399625588dc3e51d0def46cab850ed7d34d14837
bauner <bauner@gmx.de>
2015-05-17 08:44:30 +0200

    merged cm changes

--- config/common.mk
+++ config/common.mk
@@ -154,6 +154,12 @@ PRODUCT_PACKAGES += \
     CMHome \
     CyanogenSetupWizard
 
+# CM Platform Library
+PRODUCT_PACKAGES += \
+    org.cyanogenmod.platform-res \
+    org.cyanogenmod.platform \
+    org.cyanogenmod.platform.xml
+
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
     org.cyanogenmod.hardware \
