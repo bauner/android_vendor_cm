f3d91e0dd3d0c6e4e90d2b3eeb1adbb42155130a
bauner <bauner@gmx.de>
2016-10-13 16:00:24 +0200

    disable persist.sys.root_access

--- config/common.mk
+++ config/common.mk
@@ -260,7 +260,7 @@ PRODUCT_PACKAGES += \
 endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.root_access=3
+    persist.sys.root_access=0
 
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
